db8360d071578bac8084a24d0dfe86fb8a27d5be
hyhecor <hyhecor@gmail.com>
2021-11-18 12:24:16 +0900

    Update defer.h
    DEFER_BEGIN
        DoSomething
    DEFER_END

--- defer.h
+++ defer.h
@@ -5,11 +5,17 @@
 
 #include <functional>
 
-#define DEFER_BEGIN(func) \
-defer func ([&]()->void \
+#define TOKEN_PASTE(x, y) __##x##__##y##__
+#define CAT(x,y) TOKEN_PASTE(x,y)
+#define UNIQUE_DEFER_FUNC \
+    CAT(DEFER_FUNC, __LINE__)
+
+#define DEFER_BEGIN \
+defer UNIQUE_DEFER_FUNC ([&]()->void \
 {
 
-#define DEFER_END }); 
+#define DEFER_END \
+});
 
 class defer
 {
